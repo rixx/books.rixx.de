7670a9a1aabf4d8acccf0ac8087cd06215f96b93
Tobias Kunze <r@rixx.de>
2022-03-05 15:56:56 +0100

    Automatically push database on update

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
 #!/bin/zsh
-books build && chmod a+r -R _html/ && rsync -avzu --info=progress2 -h _html/* tonks:/usr/share/webapps/books # && books social
+
+set -e
+
+books build --db /home/rixx/data/dbs/books.db && chmod a+r -R _html/
+rsync -avzu --info=progress2 -h _html/* tonks:/usr/share/webapps/books
+rsync -avzu --info=progress2 -h /home/rixx/data/dbs/books.db mycroft:/usr/share/webapps/datasette/
+# books social
