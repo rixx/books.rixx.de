e7a732985c3b01b80a1fbcb42fbd9e8f9ce1fc43
Tobias Kunze <r@rixx.de>
2022-03-10 17:33:12 +0100

    Update deploy script

--- update.sh
+++ update.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-books build --db /home/rixx/data/dbs/books.db && chmod a+r -R _html/
+books build --db /home/rixx/data/dbs/books.db
+# books build
+chmod a+r -R _html/
 rsync -avzu --info=progress2 -h _html/* tonks:/usr/share/webapps/books
 rsync -avzu --info=progress2 -h /home/rixx/data/dbs/books.db mycroft:/usr/share/webapps/datasette/
 # books social
